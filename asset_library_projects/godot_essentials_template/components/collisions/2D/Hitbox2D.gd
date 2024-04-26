d2007b0a0c2df61d3acf3d1082eb573e728dba04
Noah Betzen <noah@nezteb.net>
2024-04-26 16:36:08 -0700

    Remove manually added asset lib projects

--- asset_library_projects/godot_essentials_template/components/collisions/2D/Hitbox2D.gd
+++
@@ -1,9 +0,0 @@
-@icon("res://components/collisions/2D/hitbox.svg")
-class_name Hitbox2D extends Area2D
-
-
-func _init() -> void:
-	collision_mask = 0
-	collision_layer = 64 ## Hitboxes layer
-	monitoring = false
-	monitorable = true
--- asset_library_projects/godot_essentials_template/components/collisions/3D/Hitbox3D.gd
+++
@@ -1,9 +0,0 @@
-@icon("res://components/collisions/2D/hitbox.svg")
-class_name Hitbox3D extends Area3D
-
-
-func _init() -> void:
-	collision_mask = 0
-	collision_layer = 64 ## Hitboxes layer
-	monitoring = false
-	monitorable = true
